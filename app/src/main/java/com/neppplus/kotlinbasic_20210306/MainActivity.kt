225b5e9a9b047200d7bce87a18bf8b167d8c6f6b
전은형 <ooooo5h91@gmail.com>
2021-03-06 16:21:17 +0900

    테스트버튼 해서 토스트 띄우기까짖

--- app/src/main/java/com/neppplus/kotlinbasic_20210306/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20210306/MainActivity.kt
@@ -3,12 +3,20 @@ package com.neppplus.kotlinbasic_20210306
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        toastBtn.setOnClickListener {
+
+            Toast.makeText(this, "토스트 띄우기", Toast.LENGTH_SHORT).show()
+
+        }
 
 
         testBtn2.setOnClickListener {
